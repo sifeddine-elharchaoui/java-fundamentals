c8ae4a2f7b91a0438da5166cbee0203129dcf718
EL HARCHAOUI Sif-Eddine <sifeddine.dev.elharchaoui@gmail.com>
2026-06-02 01:38:44 +0800

    feat: add MyProfile with basic Java types

---
+++ src/week1-basics/MyProfile.java
@@ -0,0 +1,21 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String fullName = "EL HARCHAOUI Sif-Eddine";
+        int age = 22;
+        String city = "China";
+        String university = "china university of petroleum";
+        double gpa = 4.0;
+        String goal = "Backend Engineer → AI Engineer → Google";
+        boolean employed = false;
+
+        System.out.println("===== MY PROFILE =====");
+        System.out.println("Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("University: " + university);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Goal: " + goal);
+        System.out.println("Employed: " + employed);
+        System.out.println("======================");
+    }
+}
